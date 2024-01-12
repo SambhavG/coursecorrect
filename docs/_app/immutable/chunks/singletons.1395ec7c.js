8a0fe3c05d81b733bf6621059b83cab93d7bc795
SambhavG <sambhavgupta159@gmail.com>
2024-01-11 22:49:34 -0800

    Build

--- docs/_app/immutable/chunks/singletons.1395ec7c.js
+++ docs/_app/immutable/chunks/singletons.1395ec7c.js
@@ -1,8 +1,8 @@
 var _a, _b;
 import { w as writable } from "./index.c467d646.js";
-const base = ((_a = globalThis.__sveltekit_l5bdm8) == null ? void 0 : _a.base) ?? "/coursecorrect";
-const assets = ((_b = globalThis.__sveltekit_l5bdm8) == null ? void 0 : _b.assets) ?? base;
-const version = "1704631419168";
+const base = ((_a = globalThis.__sveltekit_wqo5yy) == null ? void 0 : _a.base) ?? "/coursecorrect";
+const assets = ((_b = globalThis.__sveltekit_wqo5yy) == null ? void 0 : _b.assets) ?? base;
+const version = "1705042157832";
 const SNAPSHOT_KEY = "sveltekit:snapshot";
 const SCROLL_KEY = "sveltekit:scroll";
 const INDEX_KEY = "sveltekit:index";
